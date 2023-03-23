0da4eb914353389b94a71b2aa5f7eb74c06bce6a
Vishue1892 <victorsanchesbarrietos1203@gmail.com>
2023-03-23 12:00:09 -0600

    four commit

--- lib/ui/screens/inputs_screen.dart
+++ lib/ui/screens/inputs_screen.dart
@@ -1,20 +1,43 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class InputsScreen extends StatelessWidget {
+class InputsScreen extends StatefulWidget {
   const InputsScreen({super.key});
 
   @override
+  State<InputsScreen> createState() => _InputsScreenState();
+}
+
+class _InputsScreenState extends State<InputsScreen> {
+  String _userName = '';
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Inputs Screen'),
+      appBar: AppBar(
+        title: const Text('Entradas de datos por el usuario'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: const Icon(Icons.keyboard_return_outlined),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        children: [
+          _crearEntrada(),
+        ],
       ),
+    );
+  }
+
+  Widget _crearEntrada() {
+    return TextField(
+      autofocus: true,
+      textCapitalization: TextCapitalization.words,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+      ),
+      onChanged: (valor) {
+        _userName = valor;
+        print(_userName);
+      },
     );
   }
 }
