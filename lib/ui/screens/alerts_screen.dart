a8390d22e03452ef04ffc8b46f24053cfba6ddd9
Vishue1892 <victorsanchesbarrietos1203@gmail.com>
2023-03-23 11:24:13 -0600

    third commit

--- lib/ui/screens/alerts_screen.dart
+++ lib/ui/screens/alerts_screen.dart
@@ -3,11 +3,50 @@ import 'package:flutter/material.dart';
 class AlertsScreen extends StatelessWidget {
   const AlertsScreen({super.key});
 
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        barrierColor: Colors.black26,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            title: const Text('Titulo de la alerta'),
+            content: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Contenido de la alerta'),
+                FlutterLogo(
+                  size: 50.0,
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Aceptar'),
+              ),
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('Cancelar')),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Alert Screen'),
+      appBar: AppBar(
+        title: const Text('Alert Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _mostrarAlert(context),
+          child: const Text('Mostrar alerta'),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
